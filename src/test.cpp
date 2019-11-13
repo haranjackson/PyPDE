6b920a4c181f754a1b0c5ff45024f4cd72b56aeb
Haran Jackson <jackson.haran@gmail.com>
2019-11-13 10:59:54 +0000

    new test

--- src/test.cpp
+++ src/test.cpp
@@ -1,25 +1,58 @@
 #include "ader.h"
 #include <cstddef>
 
+void F(double *ret, double *Q, int d) {
+
+  double r = Q[0];
+  double ru = Q[1];
+  double rv = Q[2];
+  double rE = Q[3];
+
+  double u = ru / r;
+  double v = rv / r;
+  double E = rE / r;
+
+  double g = 1.4;
+  double p = E * r * (g - 1);
+
+  if (d == 0) {
+    ret[0] += ru;
+    ret[1] += ru * u + p;
+    ret[2] += rv * u;
+    ret[3] += rE * u + p * u;
+  } else {
+    ret[0] += rv;
+    ret[1] += ru * v;
+    ret[2] += rv * v + p;
+    ret[3] += rE * v + p * v;
+  }
+}
+
 int main() {
 
-  void (*F)(double *, double *, int) = NULL;
   void (*B)(double *, double *, int) = NULL;
   void (*S)(double *, double *) = NULL;
 
+  int nX = 30;
+  int nY = 10;
+
+  int _nX[2] = {nX, nY};
   int ndim = 2;
-  int _nX[2] = {5, 5};
-  double _u[50];
+  int V = 4;
+
+  double _u[nX * nY * V];
+  double _dX[2] = {1. / double(nX), 1. / double(nY)};
+
   double tf = 1.;
-  double _dX[2] = {0.2, 0.2};
   double CFL = 0.9;
   int _boundaryTypes[2] = {0, 0};
-  bool STIFF = true;
+
+  bool STIFF = false;
   int FLUX = 0;
   int N = 2;
-  int V = 2;
+
   int ndt = 100;
-  double _ret[5000];
+  double _ret[ndt * nX * nY * V];
 
   ader_solver(F, B, S, _u, tf, _nX, ndim, _dX, CFL, _boundaryTypes, STIFF, FLUX,
               N, V, ndt, _ret);
