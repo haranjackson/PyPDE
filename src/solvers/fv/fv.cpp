77c6b06e7c602dcd426102de2eff9fa0da59aee4
Haran Jackson <jackson.haran@gmail.com>
2019-11-23 21:32:32 +0000

    fixed fv.cpp

--- src/solvers/fv/fv.cpp
+++ src/solvers/fv/fv.cpp
@@ -1,19 +1,10 @@
 #include "fv.h"
-#include "../../grid/indexing.h"
 #include "../../poly/basis.h"
 #include "../../poly/evaluations.h"
 #include "../../types.h"
 #include "fluxes.h"
 
 #include <iostream>
-
-bool in_bounds(iVecr inds, iVecr bounds, int offset) {
-  for (int i = 0; i < inds.size(); i++) {
-    if (inds(i) + offset < 0 || inds(i) + offset >= bounds(i))
-      return false;
-  }
-  return true;
-}
 
 FVSolver::FVSolver(void (*_F)(double *, double *, double *, int),
                    void (*_B)(double *, double *, int),
@@ -93,6 +84,43 @@ void FVSolver::centers(Matr u, Matr qh, double dt) {
   }
 }
 
+void FVSolver::qh_derivatives(Matr dqh, Matr qh) {
+
+  for (int qhCell = 0; qhCell < nX2.prod(); qhCell++)
+    for (int t = 0; t < N; t++) {
+
+      int ind = (qhCell * N + t) * Nd * V;
+      MatMap qh0(qh.data() + ind, Nd, V, OuterStride(V));
+
+      for (int d = 0; d < ndim; d++) {
+        MatMap dqhd(dqh.data() + d * qh.size() + ind, Nd, V, OuterStride(V));
+        derivs(dqhd, qh0, d, DERVALS, ndim, dX);
+      }
+    }
+}
+
+void FVSolver::calculate_endpoints(Matr dqh, Matr qh0, Matr qh1, Matr q0,
+                                   Matr q1, Matr dq0, Matr dq1, int ind0,
+                                   int ind1, int d) {
+
+  endpts(q0, qh0, d, 1, ENDVALS, ndim);
+  endpts(q1, qh1, d, 0, ENDVALS, ndim);
+
+  if (secondOrder)
+    for (int d_ = 0; d_ < ndim; d_++) {
+
+      int off = d_ * dqh.size() / ndim;
+      MatMap dqh0(dqh.data() + off + ind0, Nd, V, OuterStride(V));
+      MatMap dqh1(dqh.data() + off + ind1, Nd, V, OuterStride(V));
+
+      MatMap dq0_(dq0.data() + d_ * V, Nd, V, OuterStride(ndim * V));
+      MatMap dq1_(dq1.data() + d_ * V, Nd, V, OuterStride(ndim * V));
+
+      endpts(dq0_, dqh0, d, 1, ENDVALS, ndim);
+      endpts(dq1_, dqh1, d, 0, ENDVALS, ndim);
+    }
+}
+
 void FVSolver::interfaces(Matr u, Matr qh, double dt) {
 
   FluxGenerator fluxGenerator(F, B, NODES, WGHTS, dX, V, FLUX, ndim);
@@ -103,24 +131,27 @@ void FVSolver::interfaces(Matr u, Matr qh, double dt) {
   Mat q0(Nd_, V);
   Mat q1(Nd_, V);
 
-  Mat dqh0(Nd, V);
-  Mat dqh1(Nd, V);
-  Mat dq0(Nd_, V);
-  Mat dq1(Nd_, V);
+  Mat dq0(Nd_ * ndim, V);
+  Mat dq1(Nd_ * ndim, V);
 
   iVec indsOuter = iVec::Zero(ndim); // runs over all cells in ub
   iVec indsInner = iVec::Zero(ndim - 1);
 
+  Mat dqh(ndim * qh.rows(), V);
+  if (secondOrder)
+    qh_derivatives(dqh, qh);
+
+  Indexer indexer0(nX, nX2);
+  Indexer indexer1(nX, nX2);
+
   // (i = 0 ... (nX+1); j = 0 ... (nY+1); etc
   for (int uCell = 0; uCell < nX1.prod(); uCell++) {
 
-    int uind0 = index(indsOuter, nX, -1);  // (i-1, j-2, ...)
-    int ubind0 = index(indsOuter, nX2, 0); // (i, j, ...)
-    bool inBounds0 = in_bounds(indsOuter, nX, -1);
+    indexer0.set_indices(indsOuter);
 
     for (int t = 0; t < N; t++) {
 
-      int ind0 = (ubind0 * N + t) * Nd * V;
+      int ind0 = (indexer0.qh * N + t) * Nd * V;
       MatMap qh0(qh.data() + ind0, Nd, V, OuterStride(V));
 
       for (int d = 0; d < ndim; d++) {
@@ -128,30 +159,21 @@ void FVSolver::interfaces(Matr u, Matr qh, double dt) {
         double c = dt * WGHTS(t) / (2. * dX(d));
 
         indsOuter(d) += 1;
-        int uind1 = index(indsOuter, nX, -1);
-        int ubind1 = index(indsOuter, nX2, 0);
-        bool inBounds1 = in_bounds(indsOuter, nX, -1);
+        indexer1.set_indices(indsOuter);
         indsOuter(d) -= 1;
 
-        int ind1 = (ubind1 * N + t) * Nd * V;
+        int ind1 = (indexer1.qh * N + t) * Nd * V;
         MatMap qh1(qh.data() + ind1, Nd, V, OuterStride(V));
 
-        endpts(q0, qh0, d, 1, ENDVALS, ndim);
-        endpts(q1, qh1, d, 0, ENDVALS, ndim);
-
-        // TODO: calculate derivatives of qh beforehand
-        // - they are currently calculated twice
-        derivs(dqh0, qh0, d, DERVALS, ndim, dX);
-        derivs(dqh1, qh1, d, DERVALS, ndim, dX);
-
-        endpts(dq0, dqh0, d, 1, ENDVALS, ndim);
-        endpts(dq1, dqh1, d, 0, ENDVALS, ndim);
+        calculate_endpoints(dqh, qh0, qh1, q0, q1, dq0, dq1, ind0, ind1, d);
 
         for (int idx = 0; idx < Nd_; idx++) {
 
+          // TODO: fix dq0, dq1
           if (F != NULL)
-            fluxGenerator.flux(f, q0.row(idx), q1.row(idx), dq0.row(idx),
-                               dq1.row(idx), d, secondOrder);
+            fluxGenerator.flux(
+                f, q0.row(idx), q1.row(idx), dq0.block(idx * ndim, 0, ndim, V),
+                dq1.block(idx * ndim, 0, ndim, V), d, secondOrder);
 
           if (B != NULL)
             fluxGenerator.Bint(b, q0.row(idx), q1.row(idx), d);
@@ -160,10 +182,11 @@ void FVSolver::interfaces(Matr u, Matr qh, double dt) {
           for (int i = 0; i < ndim - 1; i++)
             tmp *= WGHTS(indsInner(i));
 
-          if (inBounds0)
-            u.row(uind0) -= tmp * (b + f);
-          if (inBounds1)
-            u.row(uind1) -= tmp * (b - f);
+          if (indexer0.valid)
+            u.row(indexer0.u) -= tmp * (b + f);
+
+          if (indexer1.valid)
+            u.row(indexer1.u) -= tmp * (b - f);
 
           update_inds(indsInner, N);
         }
--- src/solvers/fv/fv.h
+++ src/solvers/fv/fv.h
@@ -1,7 +1,35 @@
 #ifndef FV_H
 #define FV_H
 
+#include "../../grid/indexing.h"
 #include "../../types.h"
+
+class Indexer {
+private:
+  iVec nX;
+  iVec nX2;
+
+  bool in_bounds(iVecr inds, iVecr bounds, int offset) {
+    for (int i = 0; i < inds.size(); i++) {
+      if (inds(i) + offset < 0 || inds(i) + offset >= bounds(i))
+        return false;
+    }
+    return true;
+  }
+
+public:
+  int u;
+  int qh;
+  bool valid;
+
+  Indexer(iVecr _nX, iVecr _nX2) : nX(_nX), nX2(_nX2) {}
+
+  void set_indices(iVecr inds) {
+    u = index(inds, nX, -1);
+    qh = index(inds, nX2, 0);
+    valid = in_bounds(inds, nX, -1);
+  }
+};
 
 class FVSolver {
 private:
@@ -32,6 +60,11 @@ private:
   Mat ENDVALS;
 
   void centers(Matr u, Matr qh, double dt);
+
+  void qh_derivatives(Matr dqh, Matr qh);
+
+  void calculate_endpoints(Matr dqh, Matr qh0, Matr qh1, Matr q0, Matr q1,
+                           Matr dq0, Matr dq1, int ind0, int ind1, int d);
 
   void interfaces(Matr u, Matr qh, double dt);
 
