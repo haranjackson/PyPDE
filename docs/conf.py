f0268b0775a53d0d17dbc6a26af5064cc435e3d7
Haran Jackson <jackson.haran@gmail.com>
2019-11-26 22:18:51 +0000

    made index master_doc

--- docs/conf.py
+++ docs/conf.py
@@ -14,21 +14,19 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'PyPDE'
 copyright = '2019, Haran Jackson'
 author = 'Haran Jackson'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -37,7 +35,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
