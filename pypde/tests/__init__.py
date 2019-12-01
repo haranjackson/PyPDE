ec35799b281cc25b8b49e63d643a5e108fc11439
Haran Jackson <jackson.haran@gmail.com>
2019-12-01 22:10:46 +0000

    added taylor-green vortex test

--- pypde/tests/__init__.py
+++ pypde/tests/__init__.py
@@ -1,4 +1,4 @@
 from pypde.tests.euler import sod_shock
 from pypde.tests.gpr import viscous_shock_gpr
-from pypde.tests.navier_stokes import viscous_shock_ns
+from pypde.tests.navier_stokes import taylor_green_vortex_2d, viscous_shock_ns
 from pypde.tests.reactive_euler import detonation_wave
--- pypde/tests/navier_stokes/__init__.py
+++ pypde/tests/navier_stokes/__init__.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
-from numpy import arange, array, dot, eye, zeros
+from numpy import arange, array, cos, dot, eye, linspace, pi, sin, zeros
+
 from pypde import pde_solver
 from pypde.tests.exact import viscous_shock_exact
 from pypde.tests.navier_stokes.system import F_navier_stokes, total_energy, γ
 
 
-def Cvec(ρ, p, v):
+def make_Q(ρ, p, v):
     """ Returns the vector of conserved variables, given the primitive variables
     """
     Q = zeros(5)
@@ -33,13 +34,56 @@ def viscous_shock_ns():
 
     u = zeros([nx, 5])
     for i in range(nx):
-        u[i] = Cvec(ρ[i], p[i], array([v[i], 0, 0]))
+        u[i] = make_Q(ρ[i], p[i], array([v[i], 0, 0]))
 
     L = [1.]
 
     ret = pde_solver(u, tf, L, F=F_navier_stokes, CFL=0.6)
 
     plt.plot(ret[-1, :, 2] / ret[-1, :, 0])
+    plt.show()
+
+    return ret
+
+
+def taylor_green_vortex_2d():
+    """ 10.1016/j.jcp.2016.02.015
+        4.10 2D Taylor-Green Vortex
+    """
+    L = [2 * pi, 2 * pi]
+
+    nx = 50
+    ny = 50
+    tf = 1
+
+    C = 100 / γ
+    ρ = 1
+    v = zeros(3)
+
+    u = zeros([nx, ny, 5])
+    for i in range(nx):
+        for j in range(ny):
+            x = (i + 0.5) * L[0] / nx
+            y = (j + 0.5) * L[1] / ny
+            v[0] = sin(x) * cos(y)
+            v[1] = -cos(x) * sin(y)
+            p = C + (cos(2 * x) + cos(2 * y)) / 4
+            u[i, j] = make_Q(ρ, p, v)
+
+    ret = pde_solver(u,
+                     tf,
+                     L,
+                     F=F_navier_stokes,
+                     CFL=0.9,
+                     N=2,
+                     boundaryTypes='periodic')
+
+    x = linspace(0, L[0], nx)
+    y = linspace(0, L[1], ny)
+
+    ut = ret[-1, :, :, 2] / ret[-1, :, :, 0]
+    vt = ret[-1, :, :, 3] / ret[-1, :, :, 0]
+    plt.streamplot(x, y, ut, vt)
     plt.show()
 
     return ret
